bbf2b09ac6e2bf2fd7bb44fbc7aff3989a995ea7
Sergio Olmos <s.olmos.pardo@gmail.com>
2022-06-29 08:00:03 +0200

    Formatted get_quantiles

--- R/get_quantiles.R
+++ R/get_quantiles.R
@@ -1,12 +1,16 @@
 get_quantiles <- function(data,
                           write = FALSE,
                           dir = ".") {
-  if (ncol(data) != 11) {
-    stop(cli::cli_alert_danger(
-        "Data should (only) contain the 11 exposure variables."))
-  }
+  check_data(data)
 
-  quantile_matrix <- apply(data, 2, stats::quantile, prob = seq(0, 1, 0.25))
+  exposures_only <- data[exposure_names_clean]
+
+  quantile_matrix <- apply(
+    exposures_only,
+    2,
+    stats::quantile,
+    prob = seq(0, 1, 0.25)
+  )
 
   quantile_matrix
 }
