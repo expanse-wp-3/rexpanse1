81537400bcd7aeb633318e9d3225e7b0b930be71
Sergio Olmos <s.olmos.pardo@gmail.com>
2022-06-29 16:07:27 +0200

    Initial main function for running full analysis

--- R/run_pipeline.R
+++ R/run_pipeline.R
@@ -1,28 +1,38 @@
-run_pipeline <- function(data,
-                         air_vars,
-                         built_vars,
-                         temp_vars,
-                         id_var,
-                         other_vars,
-                         cohort_name,
-                         write = TRUE,
-                         output_dir = ".") {
-  data_expo <- data[c(air_vars, built_vars, temp_vars)]
-  check_data(data_expo)
-  expo_quantiles <- get_quantiles(data_expo)
-  pca_summary <- get_pca(data_expo, air_vars, built_vars, temp_vars)
+run_pca <- function(data,
+                    ses_var,
+                    other_vars,
+                    cohort_name,
+                    write = TRUE,
+                    output_dir = ".") {
   rexpanse1_version <- utils::packageVersion("rexpanse1")
+  cli::cli_h1("Running analysis pipeline with rexpanse1 version {rexpanse1_version}")
+  cli::cli_alert("Checking data...")
+  check_data(data)
+
+  if (!missing(other_vars)) {
+    data <- data[c(exposure_names_clean, ses_var, other_vars)]
+  }
+
+  cli::cli_alert("Computing exposure quantiles...")
+  expo_quantiles <- get_quantiles(data)
+
+  cli::cli_alert("Computing PCA...")
+  pca <- get_pca_domains(data, summary = FALSE)
+  pca_summary <- pca[c("overall", "air", "built", "temp")]
+
+  # TODO Add modeling of SES
 
   results <- list(
     cohort = cohort_name,
     quantiles = expo_quantiles,
     pca_summary = pca_summary,
-    package_version = paste0("rexpanse-", rexpanse1_version)
+    package_version = paste0("rexpanse1-", rexpanse1_version)
   )
 
   if (write) {
     file_name <- paste0(cohort_name, "_results.rds")
     file_path <- file.path(output_dir, file_name)
+    cli::cli_alert("Writing results to {file_path}")
     saveRDS(results, file_path)
   }
 
