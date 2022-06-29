b3b8fd34a13bbc058777a2bb6b65953a2f377935
Sergio Olmos <s.olmos.pardo@gmail.com>
2022-06-29 15:59:50 +0200

    Fixed bug in check_data

--- R/check_data.R
+++ R/check_data.R
@@ -1,10 +1,17 @@
 check_data <- function(data) {
-  missing_expo_names <- expo_names[!exposure_names_clean %in% colnames(data)]
+  missing_expo_names_idx <- !exposure_names_clean %in% colnames(data)
+  missing_expo_names <- exposure_names_clean[missing_expo_names_idx]
+  if (length(missing_expo_names) > 0) {
+    stop(
+      "Some exposure variables not found. They should have names: ",
+      exposure_names_clean
+    )
+  }
 
-  complete_cases_rows <- nrow(stats::complete.cases(data[exposure_names_clean]))
-  if (complete_cases_rows != nrow(data)) {
+  complete_cases <- sum(!stats::complete.cases(data[exposure_names_clean]))
+  if (complete_cases > 0) {
     stop("Exposure variables must not contain missing values.")
   }
 
-  cli::cli_alert_success("Your data passed all tests.")
+  cli::cli_alert_success("Your data passed all tests. Producing results...")
 }
