49601e60fdceb51e1489cf21308e7f7b4ae0ce93
Sergio Olmos <s.olmos.pardo@gmail.com>
2022-06-29 17:03:15 +0200

    Documented check_data()

--- R/check_data.R
+++ R/check_data.R
@@ -1,3 +1,14 @@
+##' Checks if the data is appropriate for subsequent analysis.
+##'
+##' Checks for missing variables, missing values in exposure variables, ...
+##' @title Check the data is ready to be analyzed
+##' @param data Data frame to be passed to run_pca() `data` argument.
+##' @return NULL
+##' @author Sergio Olmos
+##' @export
+##' @examples
+##' data(exposures)
+##' check_data(exposures)
 check_data <- function(data) {
   missing_expo_names_idx <- !exposure_names_clean %in% colnames(data)
   missing_expo_names <- exposure_names_clean[missing_expo_names_idx]
