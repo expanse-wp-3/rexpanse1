95c642c7fb01f16ce2339bb80dd9242f52f7ff05
Sergio Olmos <s.olmos.pardo@gmail.com>
2022-06-29 16:00:28 +0200

    Added documentation for get_pca()

--- R/get_pca.R
+++ R/get_pca.R
@@ -1,13 +1,25 @@
-##' Computes principal components for
+##' Computes principal components for all variables in the passed data frame.
 ##'
-##' .. content for \details{} ..
+##' If `summary = TRUE` (default), the returned list will have elements
+##' `loadings` and `variance`, with the loadings and variance explained
+##' of theprincipal components.
+##' If `summary = FALSE`, an element `data` will be included in the
+##' returned list, containing a data frame with the values of the
+##' principal components for each observation in `data`,
+##' together with all the variables in the original data.
 ##' @title Compute principal component analysis
-##' @param data
-##' @param summary
-##' @param suffix
-##' @return A list.
+##' @param data Data frame with only the variables to be included in the PCA.
+##' @param summary Logical vector of length one indicating whether only
+##'   a summary of the PCA should be returned. See Details.
+##' @param suffix A character vector of length one with the suffix to
+##'   to be appended to the principal component variable names.
+##' @return A list with the PCA output.
 ##' @author Sergio Olmos
 ##' @export
+##' @examples
+##' data(exposures, package = "rexpanse1")
+##' exposures_air <- exposures[c("no2", "pm25", "bc", "o3")]
+##' get_pca(exposures_air, summary = FALSE, suffix = "air")
 get_pca <- function(data, summary = TRUE, suffix) {
   pca <- stats::prcomp(data, center = TRUE, scale. = TRUE)
   pca_loadings <- pca$rotation
