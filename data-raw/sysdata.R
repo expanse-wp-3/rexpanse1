b2a51f30b9735a26edb5980645f5891484efe881
Sergio Olmos <s.olmos.pardo@gmail.com>
2022-07-04 11:33:55 +0200

    Changed exposure variable names to original names

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,7 +1,7 @@
 exposure_names_clean <- c(
-  "no2", "pm25", "bc", "o3",
-  "ndvi", "imperv", "dist_water",
-  "temp_warm_mean", "temp_warm_sd", "temp_cold_mean", "temp_cold_sd"
+  "NO22010", "PM252010", "BC2010", "O32010w",
+  "ndvi_2019_scaled", "ImpSurf_2015", "Dist_water",
+  "temp_2010_warm", "temp_2010_warm_sd", "temp_2010_cold", "temp_2010_cold_sd"
 )
 
 usethis::use_data(exposure_names_clean, overwrite = TRUE, internal = TRUE)
