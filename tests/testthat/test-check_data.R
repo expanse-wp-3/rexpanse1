f3ec0277997405772cfa9c9351bf958097ea54e3
Sergio Olmos <s.olmos.pardo@gmail.com>
2022-08-31 13:02:38 +0200

    Fixed tests for check_data()

--- tests/testthat/test-check_data.R
+++ tests/testthat/test-check_data.R
@@ -1,9 +1,9 @@
 test_that("missing variable names", {
-  df_missing_expo_var <- exposures[, 1:4]
+  df_missing_expo_var <- expanse[, 1:4]
   expect_error(check_data(df_missing_expo_var))
 })
 
 test_that("missing values in exposure variables", {
-  exposures$no2[1:5] <- NA
-  expect_error(check_data(exposures))
+  expanse$no2[1:5] <- NA
+  expect_error(check_data(expanse))
 })
