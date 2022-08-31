a3499b20f563277020da3309d64c350185d66bb2
Sergio Olmos <s.olmos.pardo@gmail.com>
2022-08-31 12:58:13 +0200

    Add documentation to check_data()

--- R/check_data.R
+++ R/check_data.R
@@ -7,8 +7,18 @@
 ##' @author Sergio Olmos
 ##' @export
 ##' @examples
-##' data(exposures)
-##' check_data(exposures)
+##' library(dplyr)
+##
+##' data(expanse)
+##' check_data(expanse)
+##'
+##' expanse_invalid_column <- expanse %>%
+##'   rename(no2_2010_annual = no2)
+##' try(check_data(expanse_invalid_column))
+##'
+##' expanse_missing <- expanse %>%
+##'   mutate(ndvi = ifelse(ndvi < 1, NA, ndvi))
+##' try(check_data(expanse_missing))
 check_data <- function(data) {
   missing_expo_names_idx <- !exposure_names_clean %in% colnames(data)
   missing_expo_names <- exposure_names_clean[missing_expo_names_idx]
