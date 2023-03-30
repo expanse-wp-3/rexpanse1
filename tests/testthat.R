ac68da390d118fbe795bfc2898826e31163c291f
Fabian <fabian.coloma@isglobal.org>
2023-03-30 15:31:45 +0200

    update check data

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,13 @@ library(testthat)
 library(rexpanse1)
 
 test_check("rexpanse1")
+
+
+my_results <- run_pca(
+  data,
+  ses_var = c("ses_cat_indv", "ses_cat_area"),
+  age_var = "age",
+  cohort_name = "catalonia",
+  write = TRUE,
+  output_dir = "results"
+)
